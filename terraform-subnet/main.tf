47f61d0041e6f29f223e940190ddbcf5726a799e
Manashvee <manashvee@celebaltech.com>
2025-03-04 14:20:07 +0530

    Update in subnet module status completed v1.3

--- terraform-subnet/main.tf
+++ terraform-subnet/main.tf
@@ -1,5 +1,5 @@
 
-resource "azurerm_subnet" "example" {
+resource "azurerm_subnet" "subnet" {
   name                 = var.subnet_name
   resource_group_name  = var.resource_group_name
   virtual_network_name = var.vnet_name
